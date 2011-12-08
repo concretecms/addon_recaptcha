467ac0e84220e501f0be8f3a9dbf39e2d22b1513
Andrew Embler <andrew@concrete5.org>
2011-12-08 10:50:40 -0800

    normalization

--- packages/recaptcha/models/system/captcha/types/recaptcha/controller.php
+++ packages/recaptcha/models/system/captcha/types/recaptcha/controller.php
@@ -1,6 +1,6 @@
 <?
 
-class RecaptchaSystemCaptchaTypeController extends Object {
+class RecaptchaSystemCaptchaTypeController extends SystemCaptchaTypeController {
 	
 	public function saveOptions($args) {
 		$pkg = Package::getByHandle('recaptcha');
@@ -11,8 +11,16 @@ class RecaptchaSystemCaptchaTypeController extends Object {
 	public function display() {
 		$pkg = Package::getByHandle('recaptcha');
 		Loader::library('3rdparty/recaptchalib', 'recaptcha');
+		print '<style type="text/css">#recaptcha_table label {float: none !important;}</style>';
 		print recaptcha_get_html($pkg->config('CAPTCHA_RECAPTCHA_PUBLIC_KEY'));
 	}
+	
+	public function label() {
+		$form = Loader::helper('form');
+		print $form->label('captcha', t('Verify yourself.'));
+	}
+	
+	public function showInput() {}
 
 	public function check() {
 		$pkg = Package::getByHandle('recaptcha');
